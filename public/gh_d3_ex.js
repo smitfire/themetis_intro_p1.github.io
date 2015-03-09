bec551f8d0f4eacc0f32dc98f7d00ee31b2680ab
Nick Smit <nick.smit@gmail.com>
2015-03-09 20:35:47 +0100

    initial push

---
+++ public/gh_d3_ex.js
@@ -0,0 +1,72 @@
+function appendToPage(top_week, top_day){
+  $("#q1").html("Week <span class='day'>" + top_week + "</span> had the most commits");
+  var day = top_day.shift().replace(",","");
+  var number = top_day.shift();
+  $("#q2").html("Day with highest number of commits for the year: <span class='day'>"+ day + "</span> at a total of: <span class='number'>" + number + "</span>");
+}
+
+
+var drawGraph = function(){
+  var margin = {top: 20, right: 20, bottom: 30, left: 130},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+
+  var x = d3.scale.ordinal()
+      .rangeRoundBands([0, width], .1);
+
+  var y = d3.scale.linear()
+      .range([height, 0]);
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
+
+  var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  d3.json("graph_data.json", function(error, data) {
+    appendToPage(data.top_commit_week, data.top_commit_day);
+    data = data.graph_data;
+    x.domain(data.map(function(d) { console.log(d); return d.letter; }));
+    y.domain([0, d3.max(data, function(d) { console.log(d); return d.frequency; })]);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        // .attr("transform", "rotate(-90)")
+        .attr("y", -6)
+        .attr("dy", ".3em")
+        .style("text-anchor", "end")
+        .text("Number of Commits");
+
+    svg.selectAll(".bar")
+        .data(data)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.letter); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.frequency); })
+        .attr("height", function(d) { return height - y(d.frequency); });
+
+  });
+
+  function type(d) {
+    d.frequency = +d.frequency;
+    return d;
+  }
+}
+
+$(document).ready(drawGraph);
---
+++ res.rb
@@ -0,0 +1,63 @@
+require "json"
+require 'net/http'
+require 'open-uri'
+require 'pp'
+
+
+content = JSON.parse(open("https://api.github.com/repos/mbostock/d3/stats/commit_activity").read)
+pp content
+
+def sort_by_commit(array)
+  array.sort_by { |week| week["total"] }
+end
+
+def most_committed_week_number(array)
+  array.index(sort_by_commit(array).last) + 1
+end
+
+def highest_commit_by_day_array(array)
+  array.map.with_index{ |week, index| {number_of_commits: week["days"].sort.last, week: index+1, day: week["days"].index(week["days"].sort.last)+1 } }
+end
+
+def most_committed_day_total(array)
+  highest_commit_by_day_array(array).sort_by{ |info_hash| info_hash[:number_of_commits] }.last
+end
+
+def most_committed_day(array)
+  week_arr = [0,0,0,0,0,0,0]
+  week_strings = %w(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)
+  array.each {|week| week["days"].each_with_index{|day,index| week_arr[index]+=day} }
+  Hash[week_strings.zip(week_arr)].sort_by{|key, val| val }
+end
+
+def json_to_export(array)
+  hash = {graph_data: [], top_commit_week: most_committed_week_number(array),  top_commit_day: most_committed_day(array).last}
+  most_committed_day(array).each do |day|
+    hash[:graph_data] << { letter: day.first.gsub(",", ""),  frequency: day.last }
+  end
+  hash
+end
+
+def export_json(array)
+  val = json_to_export(array)
+  File.write("public/graph_data.json", val.to_json)
+end
+
+export_json(content)
+
+p "Number of hashes from api repsonse. Should be 52 / true"
+p content.length
+p content.length == 52
+p "="*20
+p "Week number with most commits"
+p most_committed_week_number(content)
+p "="*20
+p "Day number with most commits"
+p most_committed_day_total(content)
+p "="*20
+p "Day in each week with most commits"
+v = most_committed_day(content).last
+day = v.first
+num_coms = v.last
+p "#{day} with #{num_coms} commits"
+p "="*20
